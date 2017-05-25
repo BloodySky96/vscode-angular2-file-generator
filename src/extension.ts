d57f2185f084d0d4356dd8820d34afd3fd1f8fa2
songhoon <songhoon@Kangui-MacBook-Pro.local>
2017-05-25 13:00:16 +0900

    updating config properties

--- src/extension.ts
+++ src/extension.ts
@@ -17,10 +17,10 @@ export function activate(context: vscode.ExtensionContext) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.genAngular2ComponentFiles', (uri) => {
+    let disposable = vscode.commands.registerCommand('extension.generateAngular2Files', (uri) => {
         // The code you place here will be executed every time your command is executed
 
-        let configPrefix: String = 'ng2ComponentGenerator';
+        let configPrefix: String = 'ng2FilesGenerator';
         let _workspace = vscode.workspace;
         let defaultConfig: Config = FileHelper.getDefaultConfig();
         let userConfig: Config = <Config>_workspace.getConfiguration((configPrefix + '.config'));
